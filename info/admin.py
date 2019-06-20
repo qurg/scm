5e916ae6bd4e7d2733fa80e40a389b8adf7ea457
qurg <qurg@outlook.com>
2019-06-20 13:53:58 +0800

    edit airportcode admin , add search field.

--- info/admin.py
+++ info/admin.py
@@ -5,8 +5,9 @@ from info.models import AirportCode, Exchange, Charge
 
 
 @admin.register(AirportCode)
-class AirportAdmin(admin.ModelAdmin):
+class AirportCodeAdmin(admin.ModelAdmin):
     list_display = ('airport_code', 'country_code', 'city_code', 'airport_en_name', 'airport_cn_name')
+    search_fields = ['airport_code', 'country_code', 'city_code', 'airport_en_name', 'airport_cn_name']
 
 
 @admin.register(Exchange)
